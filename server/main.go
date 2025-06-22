975b31985ca2d8bb2a44b65685b2a8b90e2c5bbe
virtualzone <heiner.beck@outlook.com>
2025-06-22 16:31:40 +0200

    feat: support caching via Valkey.io

--- server/main.go
+++ server/main.go
@@ -20,6 +20,15 @@ func main() {
 	a.InitializePlugins()
 	a.InitializeRouter()
 	a.InitializeTimers()
+
+	res, err := GetCache().Get("blubb")
+	log.Println("Cache result:", string(res), "Error:", err)
+
+	log.Println(GetCache().Set("blubb", []byte("blabb"), 60))
+
+	res, err = GetCache().Get("blubb")
+	log.Println("Cache result:", string(res), "Error:", err)
+
 	a.Run()
 	db.Close()
 	os.Exit(0)
--- server/repository/cache.go
+++ server/repository/cache.go
@@ -1,10 +1,15 @@
 package repository
 
 import (
+	"context"
 	"errors"
+	"log"
 	"sync"
+	"time"
 
 	"github.com/coocood/freecache"
+	"github.com/seatsurfing/seatsurfing/server/config"
+	"github.com/valkey-io/valkey-go"
 )
 
 type Cache struct {
@@ -16,14 +21,30 @@ var cacheOnce sync.Once
 
 func GetCache() *Cache {
 	cacheOnce.Do(func() {
-		cache = &Cache{
-			Cache: freecache.NewCache(10 * 1024 * 1024), // 10 MB cache
+		if config.GetConfig().CacheType == "default" {
+			cache = &Cache{
+				Cache: freecache.NewCache(10 * 1024 * 1024), // 10 MB cache
+			}
 		}
 	})
 	return cache
 }
 
 func (c *Cache) Set(key string, value []byte, expireSeconds int) error {
+	if config.GetConfig().CacheType == "valkey" {
+		client, err := c.getValkeyClient()
+		if err != nil {
+			return errors.New("failed to get valkey client")
+		}
+		defer client.Close()
+		ctx := context.Background()
+		b := client.B().Set().Key(key).Value(string(value)).Ex(time.Duration(expireSeconds) * time.Second).Build()
+		if err := client.Do(ctx, b).Error(); err != nil {
+			return errors.New("failed to set cache")
+		}
+		return nil
+	}
+
 	if err := c.Cache.Set([]byte(key), value, expireSeconds); err != nil {
 		return errors.New("failed to set cache")
 	}
@@ -31,6 +52,21 @@ func (c *Cache) Set(key string, value []byte, expireSeconds int) error {
 }
 
 func (c *Cache) Get(key string) ([]byte, error) {
+	if config.GetConfig().CacheType == "valkey" {
+		client, err := c.getValkeyClient()
+		if err != nil {
+			return nil, errors.New("failed to get valkey client")
+		}
+		defer client.Close()
+		ctx := context.Background()
+		b := client.B().Get().Key(key).Build()
+		res, err := client.Do(ctx, b).ToString()
+		if err != nil {
+			return nil, errors.New("cache miss")
+		}
+		return []byte(res), nil
+	}
+
 	value, err := c.Cache.Get([]byte(key))
 	if err != nil {
 		return nil, errors.New("cache miss")
@@ -39,6 +75,39 @@ func (c *Cache) Get(key string) ([]byte, error) {
 }
 
 func (c *Cache) Delete(key string) error {
+	if config.GetConfig().CacheType == "valkey" {
+		client, err := c.getValkeyClient()
+		if err != nil {
+			return errors.New("failed to get valkey client")
+		}
+		defer client.Close()
+		ctx := context.Background()
+		b := client.B().Del().Key(key).Build()
+		if err := client.Do(ctx, b).Error(); err != nil {
+			return errors.New("failed to delete cache")
+		}
+		return nil
+	}
+
 	c.Cache.Del([]byte(key))
 	return nil
+}
+
+func (c *Cache) getValkeyClient() (valkey.Client, error) {
+	cfg := config.GetConfig()
+	client, err := valkey.NewClient(valkey.ClientOption{
+		InitAddress: []string{cfg.ValkeyHost},
+		AuthCredentialsFn: func(ctx valkey.AuthCredentialsContext) (valkey.AuthCredentials, error) {
+			// Replace with your actual authentication logic if needed
+			return valkey.AuthCredentials{
+				Username: cfg.ValkeyUsername,
+				Password: cfg.ValkeyPassword,
+			}, nil
+		},
+	})
+	if err != nil {
+		log.Println("Error creating Valkey client:", err)
+		return nil, err
+	}
+	return client, nil
 }
