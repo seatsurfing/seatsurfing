b333244a7c3fb48f12dc0673b90f7f631d710a2a
virtualzone <heiner.beck@outlook.com>
2025-06-28 22:15:02 +0200

    fix

--- e2e/tests/booking-ui.spec.ts
+++ e2e/tests/booking-ui.spec.ts
@@ -21,11 +21,13 @@ test('crud booking', async ({ page }) => {
   await page.getByRole('combobox').selectOption({label: 'Sample Floor'});
   await expect(page.getByText('Loading...')).not.toBeVisible();
   await page.getByText('Desk 1', { exact: true }).click();
+  await expect(page.getByRole('dialog').getByText('Book a space')).toBeVisible();
   await page.getByRole('button', { name: 'Confirm booking' }).click();
+  await expect(page.getByRole('dialog').getByText('Your booking has been confirmed!')).toBeVisible();
   await page.getByRole('button', { name: 'My bookings' }).click();
   await expect(page).toHaveURL(/bookings\/$/);
   await expect(page.getByText('Loading...')).not.toBeVisible();
-  await page.getByText(/Sample Floor/).click();
+  await page.getByText(/Sample Floor/).first().click();
   await page.getByRole('button', { name: 'Cancel booking' }).click();
   await page.getByText('No bookings.');
   await page.getByRole('link', { name: 'Book a space' }).click();
