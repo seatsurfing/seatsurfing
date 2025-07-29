aad847dc86314954087366fb02018db4ac74b68e
Heiner <13085858+virtualzone@users.noreply.github.com>
2025-07-29 13:59:17 +0200

    fix: allow images via https in booking ui in order to show custom logos (#1132)

--- booking-ui/src/pages/_document.tsx
+++ booking-ui/src/pages/_document.tsx
@@ -11,7 +11,7 @@ class Doc extends Document<Props> {
     const nonce = randomBytes(128).toString('base64')
     let csp = new Map<string, string[]>();
     csp.set('default-src', ["'self'"]);
-    csp.set('img-src', ["'self'", "data:", "'unsafe-eval'"]);
+    csp.set('img-src', ["'self'", "data:", "https:", "'unsafe-eval'"]);
     csp.set('style-src', ["'self'", "data:", "'unsafe-inline'"]);
     csp.set('object-src', ["data:", "'unsafe-eval'"]);
     csp.set('base-uri', ["'none'"]);
