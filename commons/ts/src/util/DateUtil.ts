3cf2eb7ebd972db942cef71bada1ec5e03344eb4
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2025-09-13 14:57:19 +0200

    pass enter and leave date from dashboard cards linking to bookings page

--- commons/ts/src/util/DateUtil.ts
+++ commons/ts/src/util/DateUtil.ts
@@ -16,4 +16,43 @@ export default class DateUtil {
 
     return s === date.toISOString().split("T")[0];
   }
+
+  static getTodayDateString(): string {
+    return this.getDateString(0);
+  }
+
+  /**
+   *
+   * @param offset Offset in days
+   * @returns return the date in format YYYY-MM-DD
+   */
+  static getDateString(offset: number): string {
+    const date = new Date();
+    date.setDate(date.getDate() + offset);
+    return date.toISOString().split("T")[0];
+  }
+
+  static getLastWeekMondayDateString(): string {
+    const d = new Date();
+    d.setDate(d.getDate() - (d.getDay() === 0 ? 6 : d.getDay() - 1) - 7);
+    return d.toISOString().split("T")[0];
+  }
+
+  static getLastWeekSundayDateString(): string {
+    const d = new Date();
+    d.setDate(d.getDate() - (d.getDay() === 0 ? 6 : d.getDay() - 1) - 1);
+    return d.toISOString().split("T")[0];
+  }
+
+  static getThisWeekMondayDateString(): string {
+    const d = new Date();
+    d.setDate(d.getDate() - (d.getDay() === 0 ? 6 : d.getDay() - 1));
+    return d.toISOString().split("T")[0];
+  }
+
+  static getThisWeekSundayDateString(): string {
+    const d = new Date();
+    d.setDate(d.getDate() - (d.getDay() === 0 ? 6 : d.getDay() - 1) + 6);
+    return d.toISOString().split("T")[0];
+  }
 }
