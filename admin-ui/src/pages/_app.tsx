069565a93158ce2123ff0848125cf463b138d3f1
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2025-09-27 21:56:15 +0200

    fix: set min-width for admin ui sidebar and fix hiding texts on mobile viewport

--- admin-ui/src/pages/_app.tsx
+++ admin-ui/src/pages/_app.tsx
@@ -7,6 +7,7 @@ import "@/styles/Login.css";
 import "@/styles/NavBar.css";
 import "@/styles/Settings.css";
 import "@/styles/SideBar.css";
+import "@/styles/FullLayout.css";
 import "react-calendar/dist/Calendar.css";
 import "@/styles/Booking.css";
 import type { AppProps } from "next/app";
