230b16739260237c5469fbaeda2b6f2791d0e1c7
virtualzone <heiner.beck@outlook.com>
2025-09-30 21:36:13 +0300

    fix: incorrect names

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname "$SCRIPT")
 cd $BASEDIR || exit 1
 
-echo "booking-ui" && \
-cd $BASEDIR/booking-ui && npm ci --force --verbose && REACT_APP_PRODUCT_VERSION=$(cat ../version.txt | awk NF) npm run build --verbose && \
+echo "ui" && \
+cd $BASEDIR/ui && npm ci --force --verbose && REACT_APP_PRODUCT_VERSION=$(cat ../version.txt | awk NF) npm run build --verbose && \
 cd $BASEDIR && \
 echo DONE
 
