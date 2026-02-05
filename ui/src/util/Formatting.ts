37601bdabf1e37399d765aa63780e6c3af06864a
virtualzone <heiner.beck@outlook.com>
2026-02-05 21:04:31 +0100

    fix: missing placeholder in delete booking confirm window

--- ui/src/util/Formatting.ts
+++ ui/src/util/Formatting.ts
@@ -142,4 +142,10 @@ export default class Formatting {
     }
     return s;
   }
+
+  static decodeHtmlEntities(text: string): string {
+    const textarea = document.createElement("textarea");
+    textarea.innerHTML = text;
+    return textarea.value;
+  }
 }
