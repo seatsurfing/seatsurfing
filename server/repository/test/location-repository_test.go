057663b58e9559e9e6908a5099be936ad617745b
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2026-03-20 20:19:16 +0100

    fix: deleting location should also delete space approvers and allowed bookers (#1876)

--- server/repository/test/location-repository_test.go
+++ server/repository/test/location-repository_test.go
@@ -23,3 +23,46 @@ func TestLocationsCount(t *testing.T) {
 	}
 	CheckTestInt(t, 1, res)
 }
+
+func TestLocationDeleteCleansUpSpaceRelations(t *testing.T) {
+	ClearTestDB()
+	org := CreateTestOrg("test.com")
+
+	l1 := &Location{OrganizationID: org.ID, Name: "L1"}
+	GetLocationRepository().Create(l1)
+
+	s1 := &Space{LocationID: l1.ID, Name: "S1"}
+	GetSpaceRepository().Create(s1)
+
+	g1 := &Group{OrganizationID: org.ID, Name: "G1"}
+	GetGroupRepository().Create(g1)
+
+	err := GetSpaceRepository().AddApprovers(s1, []string{g1.ID})
+	CheckTestBool(t, true, err == nil)
+
+	err = GetSpaceRepository().AddAllowedBookers(s1, []string{g1.ID})
+	CheckTestBool(t, true, err == nil)
+
+	approvers, err := GetSpaceRepository().GetApproverGroupIDs(s1.ID)
+	CheckTestBool(t, true, err == nil)
+	CheckTestInt(t, 1, len(approvers))
+
+	bookers, err := GetSpaceRepository().GetAllowedBookersGroupIDs(s1)
+	CheckTestBool(t, true, err == nil)
+	CheckTestInt(t, 1, len(bookers))
+
+	err = GetLocationRepository().Delete(l1)
+	CheckTestBool(t, true, err == nil)
+
+	approvers, err = GetSpaceRepository().GetApproverGroupIDs(s1.ID)
+	CheckTestBool(t, true, err == nil)
+	CheckTestInt(t, 0, len(approvers))
+
+	bookers, err = GetSpaceRepository().GetAllowedBookersGroupIDs(s1)
+	CheckTestBool(t, true, err == nil)
+	CheckTestInt(t, 0, len(bookers))
+
+	count, err := GetLocationRepository().GetCount(org.ID)
+	CheckTestBool(t, true, err == nil)
+	CheckTestInt(t, 0, count)
+}
