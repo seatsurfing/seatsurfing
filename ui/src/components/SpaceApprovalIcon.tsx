3b4b0ad463bc615bebaf4eeaa6f0e7e4653e4823
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2026-03-26 17:19:06 +0100

    fix global CSS error

--- ui/src/components/SpaceApprovalIcon.tsx
+++ ui/src/components/SpaceApprovalIcon.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { UserCheck as IconUserCheck } from "react-feather";
-import "./SpaceApprovalIcon.css";
+import styles from "./SpaceApprovalIcon.module.css";
 
 const SpaceApprovalIcon: React.FC = () => (
   <IconUserCheck
     size={16}
-    className="position-absolute top-0 end-0 space-approval-icon"
+    className={`position-absolute top-0 end-0 ${styles["space-approval-icon"]}`}
   />
 );
 
