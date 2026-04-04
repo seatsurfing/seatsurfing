4a9138347be892c20c1592e93788253c3c1a05b8
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2026-04-04 15:02:14 +0200

    fix prettier code formatting

--- e2e/util/helper.ts
+++ e2e/util/helper.ts
@@ -2,7 +2,11 @@ import { Page, expect } from "@playwright/test";
 
 const uiURL = process.env.UI_URL ? process.env.UI_URL : "http://localhost:8080";
 
-export async function login(page: Page, email: string, password: string): Promise<void> {
+export async function login(
+  page: Page,
+  email: string,
+  password: string,
+): Promise<void> {
   await page.goto(uiURL + "/ui/login/");
   await expect(page).toHaveURL(/login\/$/);
   await page.getByPlaceholder("you@company.com").fill(email);
