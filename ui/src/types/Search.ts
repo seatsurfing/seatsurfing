bc624ba1816618bb36bd1913da598f97ca50952e
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2026-04-11 16:17:53 +0200

    add dedicated search DTO objects

--- ui/src/types/Search.ts
+++ ui/src/types/Search.ts
@@ -1,8 +1,83 @@
-import User from "./User";
-import Location from "./Location";
-import Space from "./Space";
 import Ajax from "../util/Ajax";
-import Group from "./Group";
+
+export class UserSearchResult {
+  id: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+
+  constructor() {
+    this.id = "";
+    this.email = "";
+    this.firstname = "";
+    this.lastname = "";
+  }
+
+  deserialize(input: any): void {
+    this.id = input.id;
+    this.email = input.email;
+    this.firstname = input.firstname;
+    this.lastname = input.lastname;
+  }
+}
+
+export class LocationSearchResult {
+  id: string;
+  name: string;
+  description: string;
+
+  constructor() {
+    this.id = "";
+    this.name = "";
+    this.description = "";
+  }
+
+  deserialize(input: any): void {
+    this.id = input.id;
+    this.name = input.name;
+    this.description = input.description;
+  }
+}
+
+export class SpaceSearchResult {
+  id: string;
+  name: string;
+  location: LocationSearchResult | null;
+
+  constructor() {
+    this.id = "";
+    this.name = "";
+    this.location = null;
+  }
+
+  deserialize(input: any): void {
+    this.id = input.id;
+    this.name = input.name;
+    if (input.location) {
+      const loc = new LocationSearchResult();
+      loc.deserialize(input.location);
+      this.location = loc;
+    }
+  }
+}
+
+export class GroupSearchResult {
+  id: string;
+  name: string;
+  organizationId: string;
+
+  constructor() {
+    this.id = "";
+    this.name = "";
+    this.organizationId = "";
+  }
+
+  deserialize(input: any): void {
+    this.id = input.id;
+    this.name = input.name;
+    this.organizationId = input.organizationId;
+  }
+}
 
 export class SearchOptions {
   keyword: string = "";
@@ -26,10 +101,10 @@ export class SearchOptions {
 }
 
 export default class Search {
-  users: User[];
-  locations: Location[];
-  spaces: Space[];
-  groups: Group[];
+  users: UserSearchResult[];
+  locations: LocationSearchResult[];
+  spaces: SpaceSearchResult[];
+  groups: GroupSearchResult[];
 
   constructor() {
     this.users = [];
@@ -41,28 +116,28 @@ export default class Search {
   deserialize(input: any): void {
     if (input.users) {
       this.users = input.users.map((user: any) => {
-        let e = new User();
+        const e = new UserSearchResult();
         e.deserialize(user);
         return e;
       });
     }
     if (input.groups) {
       this.groups = input.groups.map((group: any) => {
-        let e = new Group();
+        const e = new GroupSearchResult();
         e.deserialize(group);
         return e;
       });
     }
     if (input.locations) {
       this.locations = input.locations.map((location: any) => {
-        let e = new Location();
+        const e = new LocationSearchResult();
         e.deserialize(location);
         return e;
       });
     }
     if (input.spaces) {
       this.spaces = input.spaces.map((space: any) => {
-        let e = new Space();
+        const e = new SpaceSearchResult();
         e.deserialize(space);
         return e;
       });
