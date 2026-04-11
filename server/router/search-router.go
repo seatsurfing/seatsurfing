89d6b0acb48644672d03a256c1f6c42fb97e65e5
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2026-04-11 11:10:57 +0200

    fix: allow space admins to search for users

--- server/router/search-router.go
+++ server/router/search-router.go
@@ -33,7 +33,7 @@ func (router *SearchRouter) getResults(w http.ResponseWriter, r *http.Request) {
 	res := &GetSearchResultsResponse{}
 
 	if r.URL.Query().Get("includeUsers") == "1" {
-		if CanAdminOrg(user, user.OrganizationID) {
+		if CanAdminOrg(user, user.OrganizationID) || CanSpaceAdminOrg(user, user.OrganizationID) {
 			if err := router.addUserResults(user, keyword, res); err != nil {
 				log.Println(err)
 				SendInternalServerError(w)
