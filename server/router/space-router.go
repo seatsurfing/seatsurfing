4f1fd1e9808d7241e393d1579d257da403d0f5b3
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2026-04-17 20:01:22 +0200

    fix: add additional validation in space-router (#1999)

--- server/router/space-router.go
+++ server/router/space-router.go
@@ -21,11 +21,11 @@ type SpaceAttributeValueRequest struct {
 
 type CreateSpaceRequest struct {
 	Name                  string                       `json:"name" validate:"required,max=128"`
-	X                     uint                         `json:"x"`
-	Y                     uint                         `json:"y"`
-	Width                 uint                         `json:"width"`
-	Height                uint                         `json:"height"`
-	Rotation              uint                         `json:"rotation"`
+	X                     uint                         `json:"x" validate:"max=100000"`
+	Y                     uint                         `json:"y" validate:"max=100000"`
+	Width                 uint                         `json:"width" validate:"max=5000"`
+	Height                uint                         `json:"height" validate:"max=5000"`
+	Rotation              uint                         `json:"rotation" validate:"max=359"`
 	RequireSubject        bool                         `json:"requireSubject"`
 	Enabled               bool                         `json:"enabled"`
 	Attributes            []SpaceAttributeValueRequest `json:"attributes"`
@@ -341,6 +341,10 @@ func (router *SpaceRouter) IsUserAllowedToBookLocation(allowedBookers []*Locatio
 func (router *SpaceRouter) bulkUpdate(w http.ResponseWriter, r *http.Request) {
 	var m SpaceBulkUpdateRequest
 	if UnmarshalValidateBody(r, &m) != nil {
+		SendBadRequest(w)
+		return
+	}
+	if len(m.Creates)+len(m.Updates)+len(m.DeleteIDs) > 64 {
 		SendBadRequest(w)
 		return
 	}
