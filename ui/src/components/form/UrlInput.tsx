e11e37f93258f2002f62baf3abb0ee23add709d4
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2026-05-12 16:15:22 +0200

    fix prettier formatting

--- ui/src/components/form/UrlInput.tsx
+++ ui/src/components/form/UrlInput.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 
-type Props = Omit<React.ComponentProps<typeof Form.Control>, "type" | "pattern">;
+type Props = Omit<
+  React.ComponentProps<typeof Form.Control>,
+  "type" | "pattern"
+>;
 
 const UrlInput = (props: Props) => (
-  <Form.Control type="url" pattern="https?://.+" placeholder="https://..." {...props} />
+  <Form.Control
+    type="url"
+    pattern="https?://.+"
+    placeholder="https://..."
+    {...props}
+  />
 );
 
 export default UrlInput;
