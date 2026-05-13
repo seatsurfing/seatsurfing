c23d51d9044175ef137dd1e86926a0c92355a59a
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2026-05-13 07:43:57 +0200

    ci(core): enforce commitlint scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,10 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "header-max-length": [2, "always", 150],
+    "scope-enum": [
+      2,
+      "always",
+      ["booking-ui", "admin-ui", "core", "deps", "deps-dev", "i18n"],
+    ],
   },
 };
