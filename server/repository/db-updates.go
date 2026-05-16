3edc3a3032e5c109eae344439f852e9789b030ad
Heiner <13085858+virtualzone@users.noreply.github.com>
2026-05-16 18:04:21 +0200

    feat(server): support api tokens for service accounts (#2135)

--- server/repository/db-updates.go
+++ server/repository/db-updates.go
@@ -11,7 +11,7 @@ import (
 )
 
 func RunDBSchemaUpdates() {
-	targetVersion := 42
+	targetVersion := 43
 	curVersion, err := GetSettingsRepository().GetGlobalInt(SettingDatabaseVersion.Name)
 	log.Printf("Initializing database with schema version %d (current: %d) …\n", targetVersion, curVersion)
 	if err != nil {
