9479ad842fe398c069b850453d05ccd65279f975
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2026-05-20 19:12:20 +0200

    fix unit test

--- server/router/test/check-update-router_test.go
+++ server/router/test/check-update-router_test.go
@@ -1,10 +1,12 @@
 package test
 
 import (
+	"encoding/json"
 	"net/http"
 	"testing"
 
 	. "github.com/seatsurfing/seatsurfing/server/testutil"
+	. "github.com/seatsurfing/seatsurfing/server/util"
 )
 
 func TestUpdateCheckNoUpdate(t *testing.T) {
@@ -13,8 +15,12 @@ func TestUpdateCheckNoUpdate(t *testing.T) {
 	user := CreateTestUserInOrg(org)
 
 	// In test environment GetUpdateChecker().Latest will be nil since the update checker
-	// is not initialized, so the endpoint should return 404
+	// is not initialized, so the endpoint should return an empty version
 	req := NewHTTPRequest("GET", "/uc/", user.ID, nil)
 	res := ExecuteTestRequest(req)
-	CheckTestResponseCode(t, http.StatusNotFound, res.Code)
+	CheckTestResponseCode(t, http.StatusOK, res.Code)
+	var resBody CheckVersionResponse
+	json.Unmarshal(res.Body.Bytes(), &resBody)
+	CheckTestBool(t, false, resBody.UpdateAvailable)
+	CheckTestString(t, "", resBody.LatestVersion)
 }
