5a2ef1e7ba1c49f906dc4aa738a2df2bf8ad6919
virtualzone <heiner.beck@outlook.com>
2026-05-24 11:14:57 +0200

    fix(admin-ui): display cloud solution hint

---
+++ ui/src/components/CloudHint.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { TranslationFunc, withTranslation } from "./withTranslation";
+import RuntimeConfig from "./RuntimeConfig";
+import { Alert, Col, Row } from "react-bootstrap";
+
+interface State {}
+
+interface Props {
+  t: TranslationFunc;
+}
+
+class CloudHint extends React.Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    if (RuntimeConfig.INFOS.cloudHosted) {
+      return <></>;
+    }
+    return (
+      <Row className="mb-4">
+        <Col sm="8">
+          <Alert variant="info">
+            <p>💎 {this.props.t("cloudHint")}</p>
+            <p>
+              🚀{" "}
+              <a
+                href="https://seatsurfing.io/sign-up?paid"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {this.props.t("cloudHintLink")}
+              </a>
+            </p>
+          </Alert>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default withTranslation(CloudHint as any);
