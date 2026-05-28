af557ed99a422795fca7cb3cf5bb14b9fc0d2929
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2026-05-28 20:43:53 +0200

    fix(main): fix window check in BrowserUtil (#2227)

--- ui/src/util/BrowserUtil.ts
+++ ui/src/util/BrowserUtil.ts
@@ -3,7 +3,8 @@ export default class BrowserUtil {
   static LOCAL_STORAGE_KEY_MY_BOOKINGS_VIEW = "myBookingsListView";
 
   static tryLocalStorageSetItem(key: string, value: string): boolean {
-    if (window === undefined || window.localStorage === undefined) return false;
+    if (typeof window === "undefined" || window.localStorage === undefined)
+      return false;
     try {
       window.localStorage.setItem(key, value);
     } catch {
@@ -13,7 +14,7 @@ export default class BrowserUtil {
   }
 
   static tryLocalStorageGetItem(key: string, defaultValue: any): any {
-    if (window === undefined || window.localStorage === undefined)
+    if (typeof window === "undefined" || window.localStorage === undefined)
       return defaultValue;
     try {
       return window.localStorage.getItem(key) ?? defaultValue;
