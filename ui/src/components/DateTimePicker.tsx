14930ed589c1d2db8fc6d0f4539add1abbc00bed
Jan Jonas <1097370+janjonas@users.noreply.github.com>
2026-05-29 10:32:35 +0200

    fix(booking-ui): make Monday always first day of week

--- ui/src/components/DateTimePicker.tsx
+++ ui/src/components/DateTimePicker.tsx
@@ -85,7 +85,7 @@ class DateTimePicker extends React.Component<Props, State> {
           time_24hr: RuntimeConfig.INFOS.use24HourTime,
           minDate: this.props.minDate,
           maxDate: this.props.maxDate,
-          locale: this.state.locale,
+          locale: { ...this.state.locale, firstDayOfWeek: 1 },
           noCalendar: this.props.noCalendar,
         }}
       />
